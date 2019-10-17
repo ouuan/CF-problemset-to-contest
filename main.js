4ce41c573729660901f3cc4523a4c4b729d78af9
ouuan <y___o___u@126.com>
2019-10-17 19:31:07 +0800

    :sparkles:(js) add link replace

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 // ==UserScript==
 // @name         CF problemset to contest
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      2.0
 // @description  Display CF problems in the contest field instead of problemset
 // @author       ouuan
-// @match        https://codeforces.com/problemset/problem/*
-// @match        https://codeforc.es/problemset/problem/*
+// @match        https://codeforces.com/problemset*
+// @match        https://codeforc.es/problemset*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
     var matches = window.location.href.match(/\/problemset\/problem\/([1-9][0-9]*)\/([A-Z][1-9]?)/);
-    if (matches.length == 3) window.location.replace("https://codeforces.com/contest/" + matches[1] + "/problem/" + matches[2]);
+    if (matches) window.location.replace("https://codeforces.com/contest/" + matches[1] + "/problem/" + matches[2]);
+
+    var links = document.querySelectorAll('a');
+
+    for (var link of links) {
+        matches = link.href.match(/\/problemset\/problem\/([1-9][0-9]*)\/([A-Z][1-9]?)/);
+        if (matches) link.href = ("https://codeforces.com/contest/" + matches[1] + "/problem/" + matches[2]);
+    }
 })();
